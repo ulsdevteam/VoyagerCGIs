71a09a64445fc95b42b488297af3ce908bd90abb
Alex Wreschnig <asw76@pitt.edu>
2019-06-20 10:04:51 -0400

    Pulled configuration settings into their own configuration file. Added config file to gitignore.

---
+++ lcsu_callslip_config.EXAMPLE.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+	/*
+	 * Printer Settings
+	 * 
+	*/
+	"printer_path" => "path.to.printer",
+	"printer_port" => 0,
+	"printer_queue" => "QUEUE_NAME",
+
+	/*
+	 * Database Settings
+	 */
+	"db_protocol" => "TCP",
+	"db_host" => "path.to.host",
+	"db_port" => 0,
+	"db_sid" => "sid",
+	"db_user" => "username",
+	"db_pass" => "password",
+
+];
+
+?>
